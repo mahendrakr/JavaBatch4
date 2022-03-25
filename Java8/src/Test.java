1b463bc466a6a95ce678920dcfc736625af3f9a0
mahendrakr <mahendrakumar7028@gmail.com>
2022-03-24 22:59:12 -0700

    variable declaration, object, method. comitted by Akshat.

--- Java8/src/Test.java
+++ Java8/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-    System.out.println("My first program");
+    System.out.println("My first java program");
 	}
 
 }
--- java8/src/javabasics/Dog.java
+++ java8/src/javabasics/Dog.java
@@ -1,5 +1,52 @@
 package javabasics;
 
+/**
+ * name-moti color-white breed-pomarian age-9 gender-M height-3ft weight-8.5kg
+ * is healthy-yes
+ * 
+ * run eat bark
+ * 
+ * @author akjai
+ *
+ */
 public class Dog {
+	String name = "Moti";
+	String color = "White";
+	String breed = "pomarian";
+	int age = 9;
+	char gender = 'M';
+	int height = 3;
+	double weight = 8.5;
+	boolean isHealthy = true;
+
+	void run() {
+		System.out.println("fast");
+	}
+
+	void eat() {
+		System.out.println("meat egg");
+	}
+
+	void bark() {
+		System.out.println("bhau bhau");
+	}
+
+	public static void main(String[] args) {
+		// create an object
+		Dog dog = new Dog();
+		// call a variable or a method
+		System.out.println(dog.name);
+		System.out.println(dog.color);
+		System.out.println(dog.breed);
+		System.out.println(dog.age);
+		System.out.println(dog.gender);
+		System.out.println(dog.height);
+		System.out.println(dog.isHealthy);
+		System.out.println(dog.weight);
+		dog.run();
+		dog.eat();
+		dog.bark();
+
+	}
 
 }
