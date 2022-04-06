9f4cfe8e15123cbedbf9da30e1418a0085b7e9d9
mahendrakr <mahendrakumar7028@gmail.com>
2022-04-05 22:57:51 -0700

    Execution order committed by Akshat

---
+++ java8/src/javabasics/Block.java
@@ -0,0 +1,26 @@
+package javabasics;
+/**
+ * You cannot access instance variable , method from static context.
+ * But you can access static variable from instance context.
+ * 
+ * @author akjai
+ *
+ */
+public class Block {
+	int a;
+	static int b;
+	
+	
+	{
+		System.out.println(b);
+		System.out.println("instance Block"); //instance block
+		
+	}
+	
+	static {
+		//System.out.println(a);
+		System.out.println("static Block"); 
+	} 
+	
+	
+}
---
+++ java8/src/javabasics/ExecutionOrder.java
@@ -0,0 +1,63 @@
+package javabasics;
+/**
+ * 1.Static things get memory first.(at the class loading time)
+ * 2.static variable or static block (execution order first come first serve )
+ * 3.instance things get memory at the time of object creation.
+ * 4.instance variable or instance block (execution order first come first serve)
+ * 5.then constructor
+ * @author akshat
+ *
+ */
+public class ExecutionOrder {
+	static {
+		System.out.println("static block 1");
+	}
+	{
+		System.out.println("instance block 2");
+	}
+	int a = m3();
+	int b;
+	static int c = m1();
+	static int d;
+	{
+		System.out.println(b);
+		System.out.println("instance block 1");
+	}
+	
+
+	
+	static {
+		System.out.println("static block 2");
+	}
+
+	ExecutionOrder() {
+		System.out.println("constructor");
+	}
+
+	public static int m1() {
+		System.out.println("m1()");
+		return 10;
+	}
+
+	public static void m2() {
+		System.out.println("m2()");
+
+	}
+
+	public int m3() {
+		System.out.println("m3()");
+		return 30;
+	}
+
+	public void m4() {
+
+		System.out.println("m4()");
+	}
+
+	public static void main(String[] args) {
+  ExecutionOrder eo=new ExecutionOrder();
+	}
+	{
+		System.out.println("instance block ");
+	}
+}
