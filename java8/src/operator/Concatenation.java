8da397e8285d75b4703f6c3df8f2e309c3744513
mahendrakr <mahendrakumar7028@gmail.com>
2022-04-26 22:12:06 +0530

    Committed by subodh.

---
+++ java8/src/operator/Concatenation.java
@@ -0,0 +1,24 @@
+package operator;
+/**Q:1>> Is java object oriented language?
+ * ans> yes
+ * 
+ * Q:2>> Is Java purely object oriented language ?
+ * ans>> No
+    
+    Q3:>> why Java is not purely object oriented language?
+    ans>i> It supports operator overloading 
+       ii> It also support primitive data type. 
+ * 
+ * 
+ *
+ */
+
+public class Concatenation {
+
+	public static void main(String[] args) {
+     System.out.println(10+20+30);
+     System.out.println("Subodh"+20+30);
+     System.out.println(10+20+"Addi"+30+40);
+	}
+
+}
---
+++ java8/src/operator/RelationalOperator.java
@@ -0,0 +1,60 @@
+package operator;
+/**Relational Operator(>,<,>=,<=)
+ * Note---> You can apply relational operator on every primitive type except boolean type and object type.
+ * 
+ *
+ */
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+    String s1="addi";
+    String s2="Subodh";
+  //  System.out.println(s1>s2); you cannot apply relational operator on object type.
+      char a= 20;
+      char b=30;
+      char c=65535;
+      System.out.println(a>b);
+      boolean b1=true;
+      boolean b2=false;
+     // System.out.println(b1>b2); you cannot apply relational operator on boolean type.
+      //System.out.println(1<2<3); you cannot perform Nested things on relational operator.
+      char c1='A';
+      char c2='B';
+      System.out.println(c1>c2);
+      System.out.println(c1);
+      int i= c1;
+      int j=c2;
+      System.out.println(i);
+      System.out.println(j);
+      System.out.println((int)'a');
+      char c3='9';
+      int k=c3;
+      System.out.println(k);
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+	}
+
+}
