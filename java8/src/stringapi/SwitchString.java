26da35ccdeca05bbf837f169a14f7af68e555560
mahendrakr <mahendrakumar7028@gmail.com>
2022-08-16 20:22:42 +0530

    committed by subodh

---
+++ java8/src/stringapi/SwitchString.java
@@ -0,0 +1,17 @@
+package stringapi;
+/** 
+ * Q: swap the given words without using third variable.
+ *  eg: String name=" Subodh Kumar"
+ *  output: Kumar Subodh.
+ *
+ */
+
+public class SwitchString {
+
+	public static void main(String[] args) {
+    String name="Subodh Kumar";
+    
+    
+	}
+
+}
